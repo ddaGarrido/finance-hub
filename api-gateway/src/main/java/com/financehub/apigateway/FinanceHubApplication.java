335bb9869c1ee949ae8560933351bbef26f9dd1a
danieldagarrido <danieldagarrido@gmail.com>
2025-10-03 21:53:54 -0300

    Minor fix and improvement

--- api-gateway/src/main/java/com/financehub/apigateway/FinanceHubApplication.java
+++ api-gateway/src/main/java/com/financehub/apigateway/FinanceHubApplication.java
@@ -5,23 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication(scanBasePackages = {
-        "com.financehub.apigateway",
-        "com.financehub.core",
-        "com.financehub.usermanagement",
-        "com.financehub.bankaccounts",
-        "com.financehub.bills"
-})
-@EntityScan(basePackages = {
-        "com.financehub.core.model",
-        "com.financehub.bills.dto",
-})
-@EnableJpaRepositories(basePackages = {
-        "com.financehub.core.repository",
-        "com.financehub.usermanagement.repository",
-        "com.financehub.bankaccounts.repository",
-        "com.financehub.bills.repository"
-})
+@SpringBootApplication(scanBasePackages = "com.financehub")
+@EnableJpaRepositories(basePackages = "com.financehub")
+@EntityScan(basePackages = "com.financehub")
 public class FinanceHubApplication {
 
     public static void main(String[] args) {
